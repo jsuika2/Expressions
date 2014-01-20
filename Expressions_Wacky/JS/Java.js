81a4c54d6fc9063a60eab4d1ae28ad0d2e5b827a
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-20 04:28:09 -0500

    can't figure out what is going on, has something to do with Strings, and multiple varriables

--- Expressions_Wacky/JS/Java.js
+++ Expressions_Wacky/JS/Java.js
@@ -3,16 +3,28 @@
  */
 
 
-var milkDrankJohn = prompt ("How many oz. does Carter drink daily?",16);
+var milkDrankJohn = prompt ("How many oz. does John drink daily?",10); //going to be the first part of the formula, add his milk drunk and her's
 
-var milkDrankJill = prompt ("How many oz. does Jill drink daily?",16);
+
 
 
 var Gallons;
 
-Gallons = (milkDrankJohn + milkDrankJill) * (.062500);
+Gallons= milkDrankJohn * 7;
 
-console.log(gallon);
+var milkDrankJill = prompt ("How many oz. does Jill drink daily?",20);
+
+
+var Total
 
 
 
+
+
+
+
+console.log(Total);
+
+
+
+162.29 // toFixed(2)
