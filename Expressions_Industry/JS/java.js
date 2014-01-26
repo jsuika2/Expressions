571eb72a5f01e5624b03079aec25fba812b8d26c
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-26 12:15:16 -0500

    on the Array section, the prompt MyArray[1] was in quotes, so it could not be uesed in math

--- Expressions_Industry/JS/java.js
+++ Expressions_Industry/JS/java.js
@@ -7,6 +7,23 @@
 
 
 
-//Strings, just trying to figure it out;
+//Array
 
-var
+
+var myArray = new Array();
+
+
+myArray[0] =45
+myArray[1] ="John";
+myArray[2] ="123";
+myArray[3] =4;
+myArray[4] ="Smith"
+
+var volume = myArray[0]* myArray[3] *.4567;
+
+
+
+alert(myArray[1]+ " "+myArray[4]);
+
+alert(volume)
+
--- Expressions_Wacky/JS/Java.js
+++ Expressions_Wacky/JS/Java.js
@@ -4,13 +4,6 @@
     //going to add how much energy drink John and Jill drank, add them up together. Multiply that number by .0625 that equals what the number is in pounds.
 
 
-var myArray =new Array ();
-myArray[0] = "milkDrankJohn";
-myArray[1] = "milkDrankJill";
-myArray[2] =  "days"
-myArray[3] = .0625                          //this is what you multiply to convert oz. to gallons
-
-
 
 var milkDrankJohn = prompt ("How many oz. does John drink daily?",10); //going to be the first part of the formula, add this to the girl's
 var milkDrankJill = prompt ("How many oz. does Jill drink daily?",20);
@@ -18,10 +11,25 @@ var days          = prompt ("How many days are energy drink needed?",5);
 
 
 
-var total = "(myArray[0]+myArray[1])* myArray[2] *myArray[3]";   //does not work bt tried it;
+var myArray =new Array ();
+myArray[0] = milkDrankJohn;
+myArray[1] = milkDrankJill;
+myArray[2] =  days
+myArray[3] = .0625                          //this is what you multiply to convert oz. to gallons
 
 
-console("total");
+
+
+
+
+
+
+var total;
+
+total = ((myArray[0]+myArray[1]))* myArray[2] * myArray[3];   //does not work bt tried it;
+
+
+console.log(total);
 
 
 
@@ -35,12 +43,3 @@ Gallons= milkDrankJohn * 7;
 
 
 
-
-
-
-
-console.log(Gallons);
-
-
-
-162.29 // toFixed(2)
