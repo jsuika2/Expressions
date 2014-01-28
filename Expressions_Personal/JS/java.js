7e2954c996da3f98c64449339065abb9203409ee
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-28 09:41:29 -0500

    calulate the expressions, results add up right.

--- Expressions_Personal/JS/java.js
+++ Expressions_Personal/JS/java.js
@@ -3,7 +3,26 @@
     Finished on 1/28/2014
  */
 
+//Going to see how how many miles a friend and me run. I will convert that formula to kilometers. And see if he got to his goal of 20 kilometers in the week.
 
 
 alert("I have a friend who is on holiday from Europe. He loves to run for exercise")
-alert("We will see how much sugar you can consume in week...")  
+alert("He likes to run 20 kilometers each week, but he needs help to convert miles to kilometers for our group run.")
+
+var dayOne =prompt("How many miles did he run on Day 1?","");
+var dayTwo =prompt("How many miles did he run on Day 2?","");
+var dayThree =prompt("How many miles did he run on Day 3?","");
+var dayFour=prompt("How many miles did he run on Day 4?","");
+var meter=.62137;
+
+var total =( (dayOne/meter)+(dayTwo /meter)+(dayThree/meter)+ (dayFour/meter))
+
+
+num = total;
+result = num.toFixed(1);
+
+
+
+console.log(total);
+console.log("He ran a total of "+result+" km this week.");
+
