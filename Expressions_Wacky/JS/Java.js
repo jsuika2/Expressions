0a216d3ec635fd545454e32d21db4512393eb6e8
Your Name Heremkdir <jsuika2@fullsail.edu>
2014-01-28 01:15:13 -0500

    redid variables, and formating expressions

--- Expressions_Wacky/JS/Java.js
+++ Expressions_Wacky/JS/Java.js
@@ -10,15 +10,23 @@
 
 //Going to add up how much you spend on entertainment, and average it out, and compare it to the national average.
 
-var tvHome=prompt("Cable T.V costs on average $65.00 a month, what do you spend?","do not place $ sign");
+var myArray = new Array ();
 
-var computers=prompt("What to you spend a month on cell/smart phone?");
+myArray[0] =prompt("Cable T.V costs on average $65.00 a month, what do you spend?","do not place $ sign");
+myArray[1] =prompt("What to you spend a month on cell/smart phone?");
+myArray[2] =prompt("What do you send on any other entertainment each month? Like movies, redbox, netflix, ect.");
+myArray[3] =((myArray[0]*1)+(myArray[1]*1)+(myArray[2]*1));
 
-var entertain=prompt("What do you send on any other entertainment each month? movies, redbox, netflix, ect.");
 
+var intro="On average people spend "
+var otherPeople=175
 
 
 
 
 //test console
-console.log(tvHome);
+//console.log(tvHome);
+
+console.log(intro);
+
+console.log(myArray[3]);
